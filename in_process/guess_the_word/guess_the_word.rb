4d6cd13a37ba0bd7154f683d0f163a48fc8325a7
krillan <pushkinidze49@gmail.com>
2023-09-20 15:41:37 +0300

    gtw sol

--- in_process/guess_the_word/guess_the_word.rb
+++ in_process/guess_the_word/guess_the_word.rb
@@ -1,13 +1,33 @@
 class SecretWord
-  attr_reader :word, :list
+  attr_reader :list, :word, :counter
   def initialize
     @list = 20.times.with_object([]) do |_, res|
       res << rand(4..7).times.with_object([]){|_, arr| arr << ('a'..'z').to_a[rand(26)]}.join
     end
     @word = @list.sample
+    @counter = 0
+  end
+
+  def show_size
+    @counter += 1
+    @word.size
+  end
+
+  def show_letter(n)
+    @counter += 1
+    @word[n-1]
   end
 end
 
-sw = SecretWord.new
-p sw.list
-p sw.word
+
+
+def solution(sw)
+  p list = sw.list
+  p sw.word
+  size = sw.show_size
+  p list.select!{|word| word.size == size}
+  letters = list.map(&:chars).transpose.map.with_index(1){|a, i| [i, a, a.tally]}#.sort_by{|n, a| }
+  # letters.find{|k, v| v.}
+end
+
+p solution(SecretWord.new)
