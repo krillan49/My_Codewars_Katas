5ca2af0034329da5084d5214608ede8676e05a05
krillan <pushkinidze49@gmail.com>
2023-09-21 14:49:57 +0300

    sol up

--- in_process/guess_the_word/guess_the_word.rb
+++ in_process/guess_the_word/guess_the_word.rb
@@ -1,16 +1,12 @@
 class SecretWord
   attr_reader :list, :word, :counter
   def initialize
+    size = rand(4..7)
     @list = 20.times.with_object([]) do |_, res|
-      res << rand(4..7).times.with_object([]){|_, arr| arr << ('a'..'z').to_a[rand(26)]}.join
+      res << size.times.with_object([]){|_, arr| arr << ('a'..'z').to_a[rand(26)]}.join
     end
     @word = @list.sample
     @counter = 0
-  end
-
-  def show_size
-    @counter += 1
-    @word.size
   end
 
   def show_letter(n)
@@ -24,9 +20,7 @@ end
 def solution(sw)
   p list = sw.list
   p sw.word
-  size = sw.show_size
-  p list.select!{|word| word.size == size}
-  letters = list.map(&:chars).transpose.map.with_index(1){|a, i| [i, a, a.tally]}#.sort_by{|n, a| }
+  letters = list.map(&:chars).transpose.map{|a| a.map{|e| [e, a.count(e)]}}.transpose.sort_by{|w| [*w.map(&:last).sort]}
   # letters.find{|k, v| v.}
 end
 
