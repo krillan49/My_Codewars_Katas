f16e138a9bb372dee1c2f41d70753486867a63e3
krillan <pushkinidze49@gmail.com>
2023-09-29 14:30:09 +0300

    rc1 sample tests

---
+++ in_process/roguelike_character_1_stats_and_weapon/rc1_spec.rb
@@ -0,0 +1,73 @@
+# require './khabib_vs_tony'
+require './rc1_solution'
+require './test_solution'
+
+# include Kata
+# include Solution
+
+# ch = Character.new(name: 'Kroker', strength: 15, intelligence: 7)
+# p ch.weapon_axe_of_fire(3, 1, 0, 20)
+# puts ch.character_info
+# p ch.weapon_staff_of_water(1, 0, 2, 50)
+# puts ch.character_info
+# p ch.stats_strange_fruit(0, 2, -1)
+# puts ch.character_info
+# ch.weapon_axe_of_fire(1, 2, 1, 10)
+# p ch.bag
+# puts ch.character_info
+
+describe "Sample tests" do
+  before do
+    @sol = Solution::Character.new(name: 'Kroker', strength: 15, intelligence: 7)
+    @test = TestSolution::Character.new(name: 'Kroker', strength: 15, intelligence: 7)
+  end
+
+  def test2
+    @sol.weapon_axe_of_fire(3, 1, 0, 20)
+    @test.weapon_axe_of_fire(3, 1, 0, 20)
+  end
+
+  def test3
+    test2
+    @sol.weapon_staff_of_water(1, 0, 2, 50)
+    @test.weapon_staff_of_water(1, 0, 2, 50)
+  end
+
+  def test4
+    test3
+    @sol.stats_strange_fruit(0, 2, -1)
+    @test.stats_strange_fruit(0, 2, -1)
+  end
+
+  def test5
+    test4
+    @sol.weapon_axe_of_fire(1, 2, 1, 10)
+    @test.weapon_axe_of_fire(1, 2, 1, 10)
+  end
+
+  it "sample test 1: New character" do
+    expect(@sol.character_info).to eq @test.character_info
+  end
+
+  # Добавить тесты на вывод сообщения - нашел меч, получил прокляти
+
+  it "sample test 2: Character find weapon" do
+    test2
+    expect(@sol.character_info).to eq @test.character_info
+  end
+
+  it "sample test 3: Character find second weapon" do
+    test3
+    expect(@sol.character_info).to eq @test.character_info
+  end
+
+  it "sample test 4: Character stats modifer" do
+    test4
+    expect(@sol.character_info).to eq @test.character_info
+  end
+
+  it "sample test 5: Character enchanced weapon" do
+    test5
+    expect(@sol.character_info).to eq @test.character_info
+  end
+end
