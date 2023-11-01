5281c9c54b93438ab7a79dd331a5b03082393367
krillan <pushkinidze49@gmail.com>
2023-11-01 15:50:13 +0300

    hvt js version published

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 ## Beta
 -
-1. [Khabib vs Tony](https://www.codewars.com/kata/6506dfecaa1d5d000f791c45)
-2. [Roguelike game 1 - stats and weapon](https://www.codewars.com/kata/651bfcbd409ea1001ef2c3cb)
+1. [Khabib vs Tony (js, ruby)](https://www.codewars.com/kata/6506dfecaa1d5d000f791c45)
+2. [Roguelike game 1 - stats and weapon (ruby)](https://www.codewars.com/kata/651bfcbd409ea1001ef2c3cb)
--- completed/khabib_vs_tony/js/khabib_vs_tony.js
+++ completed/khabib_vs_tony/js/khabib_vs_tony.js
@@ -6,24 +6,24 @@ describe("Sample tests", () => {
   it("sample test 1: Khabib won by KO", () => {
     var joAndNate = "First round! The fighters are in the center, Khabib delivers a power jab! and immediately OVERHAND! Tony felt it. Ferguson tries to break the distance with a kick, but another OVERHAND! Tony staggers. Takedown, Khabib easily moved the shocked opponent, trying to make a submission, but Tony's jiujitsu works. Hammerfist, HAMMERFIST, HAMMERFIST! Tony's doing bad, he can only try to survive. Hammerfist! HAMMERFIST! HAMMERFIST! HAMMERFIST! HAMMERFIST! Ferguson sweeps and slides out, all is not lost for him! He is shaking, can he survive until the end of the round? Khabib clamps down on his jab. Jab!, OVERHAND! Knockout!!! Incredible domination, I did not expect such an easy victory for Khabib."
     assert.strictEqual(khabibVsTony(joAndNate),'Khabib won by KO');
-  })
+  });
   it("sample test 2: Tony won by submission", () => {
     var joAndNate = "Khabib tries to pull Tony to the cage, hits the jab, but misses. ELBOW! from the turn, how did he catch him! Khabib backs away like he's shocked and got a cut above his eye, blood starts to fill his eye, Tony is chasing, kick! More KICK, PUNCH! ELBOW! More PUNCH, Khabib got another cut, his whole face is in blood, he can't see anything. Khabib does a takedown and Tony closes the guard. Khabib tries to hit, but he sees almost nothing, bumps into an ELBOW! ELBOW! ELBOW! Another ELBOW! New cuts, Khabib's face becomes like a bloody mask. Tony throws a triangle, tightens, it looks like Khabib passed out, excellent submission. Yes, Tony is horrendous, the real El Cucuy."
     assert.strictEqual(khabibVsTony(joAndNate),'Tony won by submission');
-  })
+  });
   it("sample test 3: Khabib won by TKO", () => {
     var joAndNate = `Khabib starts with a jab, Tony kicks back, another jab, OVERHAND! Tony backs to the cage. Khabib corners Ferguson, takedown. Tony tries to get out but gives back, hammerfist, HAMMERFIST, HAMMERFIST! Tony rolls over and Khabib keeps hitting him with the mount, hammerfist, HAMMERFIST, HAMMERFIST! Ferguson hits with an elbow, tries to twist and improve his position, but to no avail, hammerfist, HAMMERFIST, HAMMERFIST! Siren round is over. Khabib confidently took him 10 - 9.
     Second round. Tony starts with a kick, punch, another kick, not a bad start. Imanari roll!? Why did he do it? Tony is on the bottom again, that was stupid. Khabib went back to hammering hammerfist after hammerfist, more HAMMERFIST! It looks like the error was critical. HAMMERFIST, HAMMERFIST, HAMMERFIST, HAMMERFIST, HAMMERFIST, HAMMERFIST. Tony doesn't actively defend, Big John stops the fight.`
     assert.strictEqual(khabibVsTony(joAndNate),'Khabib won by TKO');
-  })
+  });
   it("sample test 4: Tony won by decision", () => {
     var joAndNate = "It looks like the score is equal, everything will be decided by the last round. Khabib comes up with a jab, Tony hits back with a kick, another kick, Khabib catches his leg, he can’t go down, Tony hits with an elbow, another elbow. Khabib makes another attempt to move into the fight, again unsuccessfully, it seems Tony retained more strength at the end of the fight. Jab, kick, punch. These middle exchanges are clearly in favor of Tony. Khabib closes in, takedown, Tony in guard, he defends well, gets a hammerfist, but immediately responds with an elbow. Siren, let's see what the judges say."
     assert.strictEqual(khabibVsTony(joAndNate),'Tony won by decision');
-  })
+  });
   it("sample test 5: Draw", () => {
     var joAndNate = "It looks like the score is equal, everything will be decided by the last round. Khabib comes up with a jab, Tony hits back with a kick, another kick, Khabib catches his leg, he can’t go down, Tony hits with an elbow, another elbow. Khabib makes another attempt to move into the fight, again unsuccessfully, it seems Tony retained more strength at the end of the fight. Jab, kick, punch. These middle exchanges are clearly in favor of Tony. Khabib closes in, takedown, Tony is on guard, he defends well but gets a hammerfist, and another HAMMERFIST. Siren, let's see what the judges say."
     assert.strictEqual(khabibVsTony(joAndNate),'Draw');
-  })
+  });
 });
 
 describe("Random tests", () => {
@@ -44,6 +44,7 @@ describe("Random tests", () => {
       res.push(str);
       words--;
     };
+    res = res.join(' ')
     return res;
   };
 
@@ -59,7 +60,7 @@ describe("Random tests", () => {
     return upcase || exclamation ? 2 : 1;
   }
 
-  function khabibVsTony(joeAndNate){
+  function solution(joeAndNate){
     var habibPoints = 0, tonyPoints = 0, habibHp = 100, tonyHp = 100, position = 'standing';
     joeAndNate = joeAndNate.split(' ').filter(s => khabib.concat(tony).includes(s.toLowerCase().replace(/[^a-z]/g, '')))
 
@@ -102,7 +103,9 @@ describe("Random tests", () => {
   for (let n = 1; n < 101; n++) {
     it(`random test ${n}`, () => {
       var s = kataHelper();
-      assert.strictEqual(khabibVsTony(s), solution(s), `Testing for: ${JSON.stringify(s)}`);
-    })
+      assert.strictEqual(khabibVsTony(s), solution(s));
+      // assert.strictEqual(khabibVsTony(s), solution(s), `Testing for: ${JSON.stringify(s)}`);
+      // JSON.stringify(s) - выводит ввод
+    });
   }
 });
